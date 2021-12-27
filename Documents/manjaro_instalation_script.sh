c42cf5e1ae3d615a9ccab5a0b5b491878825fa57
Rafael Araujo <rafa.pc2112@gmail.com>
2021-12-26 22:42:08 -0300

    Fix checkout

--- Documents/manjaro_instalation_script.sh
+++ Documents/manjaro_instalation_script.sh
@@ -4,6 +4,7 @@ cd $HOME
 echo ".cfg" >> .gitignore
 git clone --bare git@github.com:araujorafael/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config checkout ./
 
 ### Emacs instalation
 pamac install --no-confirm \
