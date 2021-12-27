24c26aecb3cfb7a86579661ba3c3455a82146b2f
Rafael Araujo <rafa.pc2112@gmail.com>
2021-12-27 17:54:25 -0300

    Add new entries to the config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # Emacs vars
     set -g EMACSDIR $HOME/.emacs.d
     set -g DOOMDIR $HOME/.doom.d
     set -g DOOMLOCALDIR $HOME/.emacs.d/.local
 
     fish_add_path -gp $EMACSDIR/bin
 
+    # Load asdf to terminal session and add the autocomplete to fish
     source ~/.asdf/asdf.fish
-
     . ~/.asdf/plugins/java/set-java-home.fish
+
+    # Add custom alias to deal with dotfiles
+    alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+    # load ssh-agent
+    eval (ssh-agent -c)
 end
