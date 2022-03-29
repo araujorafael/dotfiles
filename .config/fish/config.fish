cf36f831876d151c6956c55f364fab9d6a2e544d
Rafael Araujo dos Santos <rafa.pc2112@gmail.com>
2022-03-29 17:57:59 -0300

    Add fish flatpack workarround

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   neofetch
-
+  set -g LANG en_US.UTF-8
   # Emacs vars
   set -g EMACSDIR $HOME/.emacs.d
   set -g DOOMDIR $HOME/.doom.d
   set -g DOOMLOCALDIR $HOME/.emacs.d/.local
 
   fish_add_path -gp $EMACSDIR/bin
+  fish_add_path -gp $HOME/bin
 
   # Load asdf to terminal session and add the autocomplete to fish
   source ~/.asdf/asdf.fish
@@ -15,6 +16,19 @@ if status is-interactive
 
   # Add custom alias to deal with dotfiles
   alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-
-  alias telegram='flatpak run org.telegram.desktop'
+  alias g='git'
 end
+
+### Fish is not POSIX complient, to flatpack app appear on KDE plasma
+# Add flatpak exports to PATH
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
+
+# Append local bin folder to PATH
+set -a PATH ~/.local/bin
